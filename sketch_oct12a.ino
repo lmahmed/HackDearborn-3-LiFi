fd4034d8e687b1985deec4a179746ba85981136c
lhammerm <156360371+lhammerm@users.noreply.github.com>
2024-10-13 00:33:31 -0400

    Add files via upload

--- sketch_oct12a.ino
+++ sketch_oct12a.ino
@@ -1,64 +1,50 @@
 int pinSense = A0;
 int delayVal = 500;
-int intensity = 200;
+int intensity = 50;
 int SrOF[4] = {1,1,1,1};
 int ErOF[4] = {0,0,0,0};
-int myDataFlag[8] = {1,0,1,0,1,0,1,0}; //flag for our only current signal data
-int myDataBuff[8] = {0,0,0,0,0,0,0,0};
-int myEnableBuff[4] = {0,0,0,0};
+int myDataFlag[8] = {1,0,0,1,1,0,0,1}; //flag for our only current signal data
+int myDataBuff[8] = {0,0,0,0,0,0,0,0};// this is where the payload is stored
+int myEnableBuff[4] = {0,0,0,0};//sliding window that detects a start of frame
 bool receiveEnable = false;
 bool frameEnd;
 
-void printArray(int array[]){
-  for(int i = 0; i < sizeof(array); i++){
+void printArray(int array[], int size){
+  for(int i = 0; i < size; i++){
     Serial.print(array[i]);
   }
 }
 
-bool compareArray(int a[], int b[]){
-  Serial.print(sizeof(a));
-  Serial.print(sizeof(b));
-if(sizeof(a) == sizeof(b)){
-  printArray(a);
-  printArray(b);
-  delay(10000000);
-  for(int i = 0; i < (sizeof(a)-1); i++){
+bool compareArray(int a[], int b[], int size){
+  for(int i = 0; i < size - 1; i++){
       if(a[i] != b[i]) return false;
-    }
-    return true;
-    Serial.println("True");
-  }  
-  return false;
+  }
+   return true;
 }
 
 //Pop off first element and append input value
-void arrayPushback(int array[], int input){
-  int arraySize = sizeof(array);
-  Serial.println(arraySize);
-  for(int i = 0; i < (arraySize - 1); i++){
+void arrayPushback(int array[], int input, int size){
+  for(int i = 0; i < (size - 1); i++){
     array[i] = array[i + 1];
   }
-  array[arraySize - 1] = input;
+  array[size - 1] = input;
 }
 //Check if SOF or EOF has been transmitted
 void startCheck(){
-  if(compareArray(SrOF, myEnableBuff)){
+  if(compareArray(SrOF, myEnableBuff, 4)){
      receiveEnable = true;
      Serial.println("SrOF = myEnableBuff");
   }
-  if(compareArray(ErOF, myEnableBuff)) {
+  if(compareArray(ErOF, myEnableBuff, 4)) {
     frameEnd = true;
     receiveEnable = false;
+     Serial.println("ErOF = myEnableBuff");
   }
 }
 
 //Check if signal data matches flag
 int frameCheck(){
-  //Serial.println("Data Buffer\n");
-  //printArray(myDataBuff);
-  //Serial.println("Dataflag\n");
-  //printArray(myDataFlag);
-  if(compareArray(myDataFlag, myDataBuff)) return 1;
+  if(compareArray(myDataFlag, myDataBuff, 8)) return 1;
   else return 0;
 }
 
@@ -69,7 +55,6 @@ void setup(){
 
 //main loop
 void loop(){
-  Serial.println("here");
 
   int senseVal = analogRead(pinSense);
   int iBit;
@@ -79,18 +64,21 @@ void loop(){
   }
   else iBit = 1;
 
-  arrayPushback(myEnableBuff, iBit);
-  printArray(myEnableBuff);
+  arrayPushback(myEnableBuff, iBit, 4);
+  // printArray(myEnableBuff, 4);
+  // Serial.print("\n");
+  printArray(myDataBuff, 8);
+   Serial.print("\n"); 
+  //Serial.print(sizeof(myDataBuff)/sizeof(myDataBuff[0]));
 
   if(receiveEnable){
-     Serial.println("Enter");
-    arrayPushback(myDataBuff, iBit);
+    arrayPushback(myDataBuff, iBit, 8);
   }
 
   if(frameCheck() == 1){ 
     Serial.println ("Signal Received\n");
+    delay(20000);
   }
-  Serial.println("here");
   startCheck();
 
   delay(delayVal);  
